35d8dadc5cc1c97dab9b6a62fe763cc8111507a0
Austin Wise <AustinWise@gmail.com>
2022-10-09 12:15:06 -0700

    init

---
+++ AppDelegate.cs
@@ -0,0 +1,14 @@
+namespace test_mac_dotnet;
+
+[Register ("AppDelegate")]
+public class AppDelegate : NSApplicationDelegate {
+	public override void DidFinishLaunching (NSNotification notification)
+	{
+		// Insert code here to initialize your application
+	}
+
+	public override void WillTerminate (NSNotification notification)
+	{
+		// Insert code here to tear down your application
+	}
+}
---
+++ Main.cs
@@ -0,0 +1,5 @@
+using test_mac_dotnet;
+
+// This is the main entry point of the application.
+NSApplication.Init ();
+NSApplication.Main (args);
